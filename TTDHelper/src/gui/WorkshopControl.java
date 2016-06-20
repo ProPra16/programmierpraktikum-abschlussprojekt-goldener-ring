057ce0987593f50d395f452b545180185caf97ef
DiggeHummel <smack11@freenet.de>
2016-06-20 15:31:40 +0200

    attribut

--- TTDHelper/src/gui/WorkshopControl.java
+++ TTDHelper/src/gui/WorkshopControl.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -33,6 +33,7 @@ public class WorkshopControl implements Initializable {
     private Stage stage;
     private Scene scene;
     private Pane root;
+    private boolean add;
 
     public WorkshopControl() {
 
