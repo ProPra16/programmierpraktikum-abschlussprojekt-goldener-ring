37570b0d0fb53fcc0108d5efe50d9747a8401342
DiggeHummel <smack11@freenet.de>
2016-06-30 17:51:20 +0200

    fit the travis ci fail--> add import fr FXMLLoader

--- TTDHelper/src/gui/WorkshopControl.java
+++ TTDHelper/src/gui/WorkshopControl.java
@@ -16,6 +16,7 @@ import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
