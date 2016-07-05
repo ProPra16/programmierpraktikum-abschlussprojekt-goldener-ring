919f0020c42612c2aa0bcd6870f578fbfeffd6fc
DiggeHummel <smack11@freenet.de>
2016-07-05 14:22:12 +0200

    Class for checking User's code

---
+++ TTDHelper/src/util/CodeCompiler.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package util;
+
+/**
+ *
+ * @author Lars
+ */
+public class CodeCompiler {
+    // soll Code des Users checken
+    
+    public static boolean isCorrect(String code, String phase){
+        return false;
+    }
+}
